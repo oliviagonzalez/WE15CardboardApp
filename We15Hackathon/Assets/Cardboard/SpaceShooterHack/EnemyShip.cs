d099e267464434b94dd3316130d2543ec7f8b1f7
Gonzalez, Olivia <olivia.gonzalez@elekta.com>
2015-10-24 02:52:48 -0500

    Imported enemy ship. Created SpaceShooterHack folder for scripts

---
+++ We15Hackathon/Assets/Cardboard/SpaceShooterHack/EnemyShip.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class EnemyShip : MonoBehaviour {
+
+    public bool UpdatePosition;
+
+    public float UpdatePositionDelay;
+    public float FirstZPosition;
+    public float MovementSpeed;
+
+    public GameObject BasicEnemyShip;
+
+    // Use this for initialization
+	void Start () {
+
+        var BasicEnemyShip = gameObject.transform.GetChild(0).gameObject;
+        UpdatePosition = true;
+        UpdatePositionDelay = 1;
+        FirstZPosition = 6;
+        MovementSpeed = 1;
+	}
+	
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (UpdatePosition == true && UpdatePositionDelay == 0)
+        {
+            BasicEnemyShip.transform.Translate(MovementSpeed, 20, 0, 0);
+        }
+
+        if (UpdatePositionDelay > 0)
+        {
+            UpdatePositionDelay -= Time.deltaTime;
+        }
+
+        if (UpdatePositionDelay < 0)
+        {
+            UpdatePositionDelay = 0;
+        }
+    }
+
+    public void ToggleVRMode()
+    {
+        Cardboard.SDK.VRModeEnabled = !Cardboard.SDK.VRModeEnabled;
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        Update();
+    }
+
+    public void SetDestroy(bool click)
+    {
+        if(click == true)
+        {
+            Destroy(BasicEnemyShip);
+        }
+        else
+        {
+            Update();
+        }
+    }
+}
